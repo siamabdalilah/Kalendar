5fa4c59556dde10932243edb302fbbe675a2dcf4
Siam Abd Al-Ilah <you@example.com>
2018-10-14 16:11:34 -0500

    Calendar nav should be working

--- buttonListeners.js
+++ buttonListeners.js
@@ -1,12 +1,61 @@
 function showreg(){
-	document.querySelector('.modal').style.display = "block";
+	document.querySelector('#registernewuser').style.display = "block";
 }
 function cancelreg(){
-	document.querySelector('.modal').style.display = "none";
+	document.querySelector('#registernewuser').style.display = "none";
 }
-
-
-
+function addev(){
+	document.querySelector('#addevent').style.display = "block";
+}
+function cancelev(){
+	document.querySelector('#addevent').style.display = "none";
+}
+function login(){
+	const username = document.querySelector("#user").value;
+	const password = document.querySelector("#pass").value;
+	const data = {'username' : username, 'password': password};
+	// document.querySelector('#username').innerHTML = "Welcome, " + username + "<br>";
+	// 			document.querySelector('#userlogin').style.display = "none";
+	// 			document.querySelector('#userinfo').style.display = "block";
+	fetch("login.php", {
+		method: 'POST',
+        body: JSON.stringify(data),
+        headers: { 'content-type': 'application/json' }
+    })
+	.then (response => response.json())
+	.then(data => {
+		if (data.success){
+			document.querySelector("#user").value = "";
+			document.querySelector("#pass").value = "";
+			document.querySelector('#username').innerHTML = "Welcome, " + username + "<br>";
+			document.querySelector('#userlogin').style.display = "block";
+			document.querySelector('#userinfo').style.display = "none";
+			fill();
+		}
+		else{
+			alert("Wrong Username/Password. Please try again.")
+		}
+	})
+	.catch(err => alert("Something went wrong. Please try again."));
+}
+function logout(){
+	// document.querySelector('#username').innerHTML = "";
+	// 		document.querySelector('#userlogin').style.display = "block";
+	// 		document.querySelector('#userinfo').style.display = "none";
+	fetch("logout.php",{
+		method: 'POST'
+	})
+	.then(response => response.json())
+	.then(res =>{
+		if (res.response){
+			document.querySelector('#username').innerHTML = "";
+			document.querySelector('#userlogin').style.display = "block";
+			document.querySelector('#userinfo').style.display = "none";
+			fill();
+		}
+	})
+	.catch(err => alert("Something went wrong. Please try again."));
+}
 
 
 
@@ -26,4 +75,10 @@ function cancelreg(){
 
 
 document.querySelector('#reg').addEventListener("click", showreg, false);
-document.querySelector('#cancel').addEventListener("click", cancelreg, false);
+document.querySelector('#cancelreg').addEventListener("click", cancelreg, false);
+
+document.querySelector('#addev').addEventListener("click", addev, false);
+document.querySelector('#cancelev').addEventListener("click", cancelev, false);
+
+document.querySelector('#log').addEventListener("click", login, false);
+document.querySelector('#logout').addEventListener("click", logout, false);
---
+++ loadmonth.js
@@ -0,0 +1,107 @@
+let currmonth=9; let curryear=2018;
+let month = new Month(curryear, currmonth);
+
+const monthsOfYear = {
+	0 : "January",
+	1 : "February",
+	2 : "March",
+	3 : "April",
+	4 : "May",
+	5 : "June", 
+	6 : "July",
+	7 : "August",
+	8 : "September",
+	9 : "October",
+	10: "November",
+	11: "December"
+};
+
+
+function fill(){
+	document.querySelector('#curMonth').innerHTML = monthsOfYear[month.month] + ", " + month.year;
+
+	let fir = month.getDateObject(1);
+
+
+	let weeks = month.getWeeks();
+	let days = weeks[0].getDates();
+	let rows = document.querySelectorAll('tr');
+	// if 7 rows found, remove last row
+	if (rows.length == 7){
+		rows[0].parentNode.removeChild(rows[0].parentNode.lastChild);
+		rows = document.querySelectorAll('tr');
+	}
+	for (let i = 1; i < 6; i++){
+		rows[i].innerHTML = "";
+	}
+
+	// Load previous month in gray
+	for (let i = 0; i < 7; i++){
+		if (days[i].valueOf() === fir.valueOf()){
+			break;
+		}
+
+		rows[1].innerHTML += "<td>" + days[i].getDate() + "<br>";
+
+		// ADD EVENTS HERE. OKAY MAYBE NOT SUCH A GOOD IDEA
+		rows[1].innerHTML += "</td>";
+		rows[1].lastChild.style.color = "rgb(150,150,150)";
+	}
+
+
+	// Load current month
+	let nex = month.nextMonth().getDateObject(0);
+	let j = 1;
+	for (let i = fir.getDate(); month.getDateObject(i).valueOf() <= nex.valueOf(); i++){
+	
+		if ((month.getDateObject(i).getDay())% 7 == 0){
+			if (i != fir.getDate()){
+				j++;
+				// if an extra row needed, add it
+				if (j == 6){
+					rows[j-1].parentNode.appendChild(document.createElement("tr"));
+					rows = document.querySelectorAll('tr');
+				}
+			}
+			
+		}
+
+		rows[j].innerHTML += "<td>" + month.getDateObject(i).getDate() + "<br>";
+
+		// ADD EVENT HERE
+
+
+		rows[j].innerHTML += "</td>";
+	}
+
+	// load next month in gray
+	for (let i = nex.getDay() + 1; i < 7; i++){
+		console.log(i);
+		rows[rows.length-1].innerHTML += "<td>" + (i - nex.getDay()) + "<br>";
+
+		// ADD EVENTS HERE
+		rows[rows.length-1].innerHTML += "</td>";
+		rows[rows.length-1].lastChild.style.color = "rgb(150,150,150)";
+	}
+}
+
+function incmonth(){
+	month = month.nextMonth();
+	fill();
+}
+function decmonth(){
+	month = month.prevMonth();
+	fill();
+}
+
+
+
+
+document.addEventListener("DOMContentLoaded", fill, false);
+document.querySelector('#prevm').addEventListener("click", decmonth, false);
+document.querySelector('#nextm').addEventListener("click", incmonth, false);
+
+
+
+
+

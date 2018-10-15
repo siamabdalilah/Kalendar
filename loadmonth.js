6f6e3f3f64468a467dcd4eb1a31a7072968d7d2a
Animan Amit <animanamit@wustl.edu>
2018-10-15 18:27:53 -0500

    asas

--- loadmonth.js
+++ loadmonth.js
@@ -85,11 +85,12 @@ function decmonth(){
 	fill();
 }
 
-// function getEventsCurrentMonth() {
-// 	const month;
+function getEventsCurrentMonth() {
+	const mon = month.month;
+	let s = "/\d{4}-(10|11|12|\d)-(0\d|1\d|2\d|30) (\d|1\d|2[0-3]):([0-5]\d|):00/m";
 
-// 	let
-// }
+	let
+}
 
 
 document.addEventListener("DOMContentLoaded", function(){

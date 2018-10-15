7c9b1395ce1e2f890ab462c7f2b44a4602ae366e
Siam Abd Al-Ilah <you@example.com>
2018-10-15 17:01:39 -0500

    Minor Corrections

--- events.js
+++ events.js
@@ -3,7 +3,7 @@ function addEvent(){
 	const time = document.querySelector("input[name='date']").value + " " + document.querySelector("input[name='time']") + ":00";
 	const tag = document.querySelector("input[name='tag']").value;
 
-	const data = {'eventTitle' : title, 'eventTime' : time, 'eventTag' : tag}
+	const data = {'title' : title, 'time' : time, 'tag' : tag}
 
 	fetch("addEvent.php", {
 		method: 'POST',

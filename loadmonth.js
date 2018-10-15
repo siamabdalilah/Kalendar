f5b5162f35a1679f9749de6b3b0c833176ecf7d4
Siam Abd Al-Ilah <you@example.com>
2018-10-15 18:43:18 -0500

    trying to fix checklog

--- loadmonth.js
+++ loadmonth.js
@@ -85,16 +85,16 @@ function decmonth(){
 	fill();
 }
 
-function getEventsCurrentMonth() {
+// function getEventsCurrentMonth() {
 
-	const mon = month.month;
-	let s = "/\d{4}-(10|11|12|\d)-(0\d|1\d|2\d|30) (\d|1\d|2[0-3]):([0-5]\d|):00/m";
+// 	const mon = month.month;
+// 	let s = "/\d{4}-(10|11|12|\d)-(0\d|1\d|2\d|30) (\d|1\d|2[0-3]):([0-5]\d|):00/m";
 
-	const month;
+// 	const month;
 
 
-	let
-}
+// 	let
+// }
 
 
 document.addEventListener("DOMContentLoaded", function(){

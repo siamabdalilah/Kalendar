cf1de139bd5fdca9638dbf45311de35a8c357a52
Siam Abd Al-Ilah <you@example.com>
2018-10-15 20:52:11 -0500

    Minor Corrections

--- addEvent.php
+++ addEvent.php
@@ -18,7 +18,7 @@ $token = $json_obj['token'];
 
 
 if (!preg_match('/(0[1-9]|1[0-2])-(\d{4})/m', $startMonth) || !preg_match('/(0[1-9]|[1-2]\d|3[0-1])/m', $startDate) || !preg_match('/(\d|1\d|2[0-3]):([0-5]\d|):00/m', $startTime)){
-	
+
 	echo json_encode(array(
 		"success" => false,
 		"message" => "Invalid timegvgvg format"
@@ -26,42 +26,48 @@ if (!preg_match('/(0[1-9]|1[0-2])-(\d{4})/m', $startMonth) || !preg_match('/(0[1
 	exit;
 }
 
-if(htmlentities($title) === ""){
-	echo json_encode(array(
-		"success" => false,
-		"message" => "Invalid title"
-	));
-	exit;
-}
-
-
-
-if (!($token === $_SESSION['token'])){
-	echo json_encode(array(
-		"success" => false,
-		"message" => "Illegal token"
-	));
-	exit;
-}
-
-
-$stmt = $mysqli->prepare('insert into events (title, tags, username, startmonthy, startdate, starttime ) values (?,?,?,?,?,?)');
-if (!$stmt){
-	echo json_encode(array(
-		"success" => false,
-		"message" => "Invalid insert query"
-	));
-	exit;
-}
-
-$stmt->bind_param('ssssss', $title, $category, $_SESSION['username'], $startMonth, $startDate, $startTime);
-$stmt->execute();
-$stmt->close();
-
 echo json_encode(array(
 	"success" => true,
 	"message" => "Event Added"
 ));
+exit;
+// if(htmlentities($title) === ""){
+// 	echo json_encode(array(
+// 		"success" => false,
+// 		"message" => "Invalid title"
+// 	));
+// 	exit;
+// }
 
+
+
+// if (!($token === $_SESSION['token'])){
+// 	echo json_encode(array(
+// 		"success" => false,
+// 		"message" => "Illegal token"
+// 	));
+// 	exit;
+// }
+
+
+// $stmt = $mysqli->prepare('insert into events (title, tags, username, startmonthy, startdate, starttime ) values (?,?,?,?,?,?)');
+// if (!$stmt){
+// 	echo json_encode(array(
+// 		"success" => false,
+// 		"message" => "Invalid insert query"
+// 	));
+// 	exit;
+// }
+
+// $stmt->bind_param('ssssss', $title, $category, $_SESSION['username'], $startMonth, $startDate, $startTime);
+// $stmt->execute();
+// $stmt->close();
+
+// echo json_encode(array(
+// 	"success" => true,
+// 	"message" => "Event Added"
+// ));
+
+// exit;
 
 ?>

f16bde820f774cf32f3c624d0c50092e95668f9c
Siam Abd Al-Ilah <you@example.com>
2018-10-15 19:24:35 -0500

    Minor Corrections

--- checklog.php
+++ checklog.php
@@ -4,20 +4,7 @@ header("Content-Type: application/json");
 session_start();
 
 
-// echo json_encode(array(
-// 		"success" => false,
-// 		"message" => "Invalid timegvgvg format"
-// 	));
-// 	exit;
-
-// echo json_encode(array(
-// 		'session' => true,
-// 		'csrf' => 'fml',//$_SESSION['token'],
-// 		'user' => 'fml'//$_SESSION['username']
-// 	));
-// exit;
-
-if (isset($_SESSION['username']){
+if (isset($_SESSION['username'])){
 	echo json_encode(array(
 		'session' => true,
 		'csrf' => $_SESSION['token'],

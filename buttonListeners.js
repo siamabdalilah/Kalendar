3eda2fe0b151abaaa95551144da8280f62f0d962
Siam Abd Al-Ilah <you@example.com>
2018-10-16 20:15:30 -0500

    Minor Corrections

--- buttonListeners.js
+++ buttonListeners.js
@@ -123,7 +123,7 @@ function populate(){
 
 				const ent = Object.entries(day);
 				for (const [id, tag, title, startTime] of ent){
-					console.log(startTime);
+					console.log(String(startTime);
 					cell.innerHTML += "&bull; " + startTime + ": " + title + "<br>"; 
 				}
 			}

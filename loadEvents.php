ba7746752fbb1ab4e61d5953f212467063acb3c4
Siam Abd Al-Ilah <you@example.com>
2018-10-16 19:22:19 -0500

    Minor Corrections

--- loadEvents.php
+++ loadEvents.php
@@ -1,16 +1,16 @@
 <?php
 require 'database.php';
 
-header("Content-Type: application/json");
-session_start();
+// header("Content-Type: application/json");
+// session_start();
 
 
-$json_str = file_get_contents('php://input');
-$json_obj = json_decode($json_str, true);
+// $json_str = file_get_contents('php://input');
+// $json_obj = json_decode($json_str, true);
 
-$monthy = $json_obj["monthy"];
+$monthy = '10-2018';//$json_obj["monthy"];
 
-$stmt1 = $mysqli->prepare("SELECT * from events where startmonthy=$monthy and username = $_SESSION['username'] order by startdate, starttime asc");
+$stmt1 = $mysqli->prepare("SELECT * from events where startmonthy=$monthy and username = 'ani' order by startdate, starttime asc");
 if (!$stmt) {
 	echo json_encode(array(
 		"success" => false,
@@ -25,7 +25,6 @@ $stmt1->bind_result($id, $tag, $u, $title, $startdate, $startmonthy, $starttime)
 
 $monthsEvents = array();
 
-$monthsEvents[$startdate]
 
 
 while ($stmt1->fetch()){
--- test.php
+++ test.php
@@ -1,5 +1,7 @@
 <?php
 
-echo date("ddscsdc");
+$v = array('a' => 'id', 'b' => array(array('a' => 'b', 'b' => 'c'), array('a' => 'b')));
+
+echo json_encode($v, JSON_FORCE_OBJECT);
 
 ?>
